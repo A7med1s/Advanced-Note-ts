8f9906bfe2574eedac7b1281e4c5da895b99854f
ahmed samy <ahmed12samy120@gmail.com>
2024-07-01 18:27:45 +0300

    note ts

---
+++ src/components/ImageUploader.tsx
@@ -0,0 +1,44 @@
+import  { useState, ChangeEvent, useEffect } from 'react';
+const ImageUploader = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // تحقق إذا كان هناك صورة مخزنة في localStorage
+    const storedImage = localStorage.getItem('user-img');
+    if (storedImage) {
+      setImage(storedImage);
+    }
+  }, []);
+
+  useEffect(() => {
+    // تخزين قيمة الصورة في localStorage عندما تتغير
+    if (image) {
+      localStorage.setItem('user-img', image);
+    }
+  }, [image]);
+
+  const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className='img-uploader-div'>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {image && (
+        <div className='text-center'>
+         
+          <img src={image} alt="Uploaded" style={{ maxWidth: '50%', height: 'auto' }} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageUploader;
